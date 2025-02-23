60c6e21a5189e104ea161ffdc18e9cd390099227
Marcio de Andrade <marcio@requestdev.net>
2025-02-23 16:53:39 -0400

    Configuração string de conexão carrega dados

--- WindowsFormsCRUD/Form1.cs
+++ WindowsFormsCRUD/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsCRUD
 {
     public partial class Form1 : Form
     {
+        //declaração das variáveis globais
+        SqlDataReader dr;
+        SqlConnection con;
+        SqlCommand cmd;
+        string sqlstr;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //acesso - conexão e abrir banco de dados
+            con = new SqlConnection("DATA SOURCE=MARCIODEANDRADE; DATABASE=CRUD; INTEGRATED SECURITY=SSPI;");
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            //chama método para carregar dados
+            LoadData();
+        }
+
+        //método para carregar dados
+        private void LoadData()
+        {
+            cmd.CommandText = "SELECT Id, Nome, Cargo, Salario, Status FROM Funcionarios ORDER BY Id";
+            dr = cmd.ExecuteReader();
+            ShowData();
+        }
+
+        private void ShowData()
+        {
+            if (dr.Read())
+            {
+                txtId.Text = dr[0].ToString();
+                txtNome.Text = dr[1].ToString();
+                txtCargo.Text = dr[2].ToString();
+                txtSalario.Text = dr[3].ToString();
+                chkStatus.Checked = Convert.ToBoolean(dr[4]);
+            }
+            else
+                MessageBox.Show("Não existe dados!");
+        }
+
     }
 }
+
